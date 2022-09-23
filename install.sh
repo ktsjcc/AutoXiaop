d92a38e8bbaef10390438fa7bc55be1680cea1eb
ktsjcc <zhaoyang0906@163.com>
2022-09-23 16:56:20 +0800

    fix bugs which could lead can not use

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@ bashProfile=~/.bash_profile
 expect -v &> /dev/null;
 if [ $? -eq 0 ];then
 	echo "expect and tcl have been installed in your server, so you do not need to install again."
-	chmod +x auto;
+	chmod +x $HOME/xiaop/bin/auto;
 	#mv auto.sh /usr/bin/auto
 	#mkdir -p /usr/share/AutoXiaop/conf; cp -a ./send.conf.template /usr/share/AutoXiaop/conf/send.conf.template
 	#mkdir -p /usr/share/AutoXiaop/bin;  cp -a ./auto /usr/share/AutoXiaop/bin/auto
 	#mkdir -p /usr/share/AutoXiaop/lib;  cp -a ./func.cfg /usr/share/AutoXiaop/lib/func.cfg
-	sed -i "s|$(grep "PATH=" $bashProfile)|$(grep "PATH=" $bashProfile):$HOME/xiaop|g" $bashProfile
-        echo \alias auto="$HOME/xiaop/bin/auto|grep -v password" >> $bashProfile
+	sed -i "s|$(grep "PATH=" $bashProfile)|$(grep "PATH=" $bashProfile):$HOME/xiaop/bin|g" $bashProfile
 	echo ">>installation finished, go to : $HOME/xiaop<<"
+	exec bash --login
+	exit;
 else
 	read -p "lack of dependencies=======>[expect], automatically install by using yum?[Y/N]:" res
 	if [ "$(tr "[:lower:]" "[:upper:]" <<<"$res")" == "Y" ]; then
