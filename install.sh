57f89509f2f1dfa12c56e62a4088848581f7abe0
ktsjcc <zhaoyang0906@163.com>
2022-09-27 15:55:33 +0800

    20220927 bug fixed

--- install.sh
+++ install.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
-bashProfile=~/.bash_profile
+
+. $HOME/xiaop/lib/vars.cfg
+. $HOME/xiaop/lib/func.cfg
+
 expect -v &> /dev/null;
+
 if [ $? -eq 0 ];then
 	echo "expect and tcl have been installed in your server, so you do not need to install again."
-	chmod +x $HOME/xiaop/bin/auto;
-	#mv auto.sh /usr/bin/auto
-	#mkdir -p /usr/share/AutoXiaop/conf; cp -a ./send.conf.template /usr/share/AutoXiaop/conf/send.conf.template
-	#mkdir -p /usr/share/AutoXiaop/bin;  cp -a ./auto /usr/share/AutoXiaop/bin/auto
-	#mkdir -p /usr/share/AutoXiaop/lib;  cp -a ./func.cfg /usr/share/AutoXiaop/lib/func.cfg
-	sed -i "s|$(grep "PATH=" $bashProfile)|$(grep "PATH=" $bashProfile):$HOME/xiaop/bin|g" $bashProfile
-	echo ">>installation finished, go to : $HOME/xiaop<<"
-	exec bash --login
-	exit;
+	installation;
+	#chmod +x $HOME/xiaop/bin/auto;
+	#grep "$HOME/xiaop/bin" $bashProfile &> /dev/null
+	#if [ $? -eq 0 ]; then
+	#	echo "" > /dev/null;
+	#else
+	
+	#	sed -i "s|$(grep "PATH=" $bashProfile)|$(grep "PATH=" $bashProfile):$HOME/xiaop/bin|g" $bashProfile
+	#	echo ">>installation finished, go to : $HOME/xiaop<<"
+	#	exec bash --login
+	#	exit;
+	#fi
 else
 	read -p "lack of dependencies=======>[expect], automatically install by using yum?[Y/N]:" res
 	if [ "$(tr "[:lower:]" "[:upper:]" <<<"$res")" == "Y" ]; then
 
 		sudo yum install -y expect;
+		installation;
 
 	elif [ "$(tr "[:lower:]" "[:upper:]" <<<"$res")" == "N" ]; then
 		
 		echo "please intall [expect] mannually then retry."	
 		exit;
 	fi
-
 fi
